4da870add43a56e6fd39b63e15aff4466842230f
Igor Melnikov <igor2354@yandex.ru>
2021-11-15 22:50:23 +0300

    first commit

---
+++ dist/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener(
+	"DOMContentLoaded",
+	function () {
+		let wrapCenterBg = document.querySelector(".page-load__group");
+
+		if (wrapCenterBg != null) {
+			wrapCenterBg.style.height = wrapCenterBg.clientWidth + 40 + "px";
+			window.addEventListener("resize", function () {
+				wrapCenterBg.style.height = wrapCenterBg.clientWidth + 40 + "px";
+			});
+		}
+	},
+	false
+);
