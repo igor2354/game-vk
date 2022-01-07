6e4def439e5dd026bca1bb7d3055c852bbca80e5
Igor Melnikov <igor2354@yandex.ru>
2022-01-07 14:22:36 +0300

    edits 18

--- dist/js/script.js
+++ dist/js/script.js
@@ -27,6 +27,64 @@ document.addEventListener(
 				false
 			);
 		}
+
+		let goroupSliderVoting = document.querySelector(".content-voting__group-slider");
+
+		if (goroupSliderVoting != null) {
+			let nextBig = goroupSliderVoting.querySelector(".voting-slider__next-big");
+			let prevBig = goroupSliderVoting.querySelector(".voting-slider__prev-big");
+
+			let sliderVoting = new Swiper(".voting-slider", {
+				slidesPerView: 1,
+				spaceBetween: 15,
+				watchOverflow: true,
+				pagination: {
+					el: ".voting-slider__pagination",
+					clickable: true,
+					renderBullet: function (index, className) {
+						return '<span class="' + className + '">' + (index + 1) + "</span>";
+					},
+				},
+
+				navigation: {
+					nextEl: ".voting-slider__next",
+					prevEl: ".voting-slider__prev",
+				},
+
+				breakpoints: {
+					570: {
+						slidesPerView: 2,
+					},
+
+					767: {
+						slidesPerView: 3,
+					},
+				},
+
+				on: {
+					lock: function (swiper) {
+						if (nextBig != null && prevBig != null) {
+							nextBig.classList.add("swiper-button-lock");
+							prevBig.classList.add("swiper-button-lock");
+						}
+					},
+
+					unlock: function (swiper) {
+						nextBig.classList.remove("swiper-button-lock");
+						prevBig.classList.remove("swiper-button-lock");
+					},
+				},
+			});
+
+			if (nextBig != null && prevBig != null) {
+				nextBig.addEventListener("click", function () {
+					sliderVoting.slideNext(300, false);
+				});
+				prevBig.addEventListener("click", function () {
+					sliderVoting.slidePrev(300, false);
+				});
+			}
+		}
 	},
 	false
 );
